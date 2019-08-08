3d8f4ddbd7e869cfac5970041f628b75019779f9
Germaine Pannell <me@germainep.com>
2019-08-07 22:25:40 -0400

    completed project and stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -3,28 +3,44 @@
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
-// myFunction();
+const myFunction = () => {
+  console.log('Function was invoked');
+};
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+const anotherFunction = param => param;
+anotherFunction('Example');
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+const add = (param1, param2) => param1 + param2;
+
+add(1, 2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+const substract = (param1, param2) => param1 - param2;
+
+substract(1, 2);
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+const exampleArray = [1, 2, 3, 4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
